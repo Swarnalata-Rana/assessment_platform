2679995401493e69457e888d925efc3cc45da544
Swarnalata Rana <swarnalatarana21@navgurukul.org>
2024-08-21 17:21:09 +0530

    timerwork

--- app/assessment/Timer.js
+++ app/assessment/Timer.js
@@ -1,17 +1,43 @@
-import React from 'react'
+// import React from 'react'
+
+// const Timer = () => {
+//     return (
+//         <>
+//             <div className='TimerSquareBOx'>
+//                 <div className='TimerCircleSquare'>
+//                     <div></div>
+//                 </div>
+//                 <div className='RemainingTimeDiv'>
+//                     Remaining Time</div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Timer;
+
+import React from 'react';
 
 const Timer = () => {
     return (
         <>
-            <div className='TimerSquareBOx'>
-                <div className='TimerCircleSquare'>
-                    <div></div>
+            <div className='TimerSquareBox'>
+                <div className='circleFirst'>
+                    <div className='circleTow'>
+                        <div className='circle'>
+                            <div className='TimerText'>30:
+                                {/* <div className=''>:</div> */}
+                                00</div>
+                            <div className='nimSec'>nims sec</div>
+                        </div>
+                    </div>
                 </div>
                 <div className='RemainingTimeDiv'>
-                    Remaining Time</div>
+                    Remaining Time
+                </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Timer;
--- app/score/page.js
+++ app/score/page.js
@@ -10,26 +10,28 @@ const page = () => {
             <div className=''>
                 <div className='mainScoreDiv'>
                     <div className='gapDiv0'></div>
-                    {/* <div className='blackDivBox0'> */}
-                        <div className='blackDivBox'>
-                            <div className=''>
-                                <p className='Tag35'>35/40</p>
-                                <p className='yourScor'>Your Scored</p>
-                            </div>
+                    <div className='blackDivBox'>
+                        <div className='mainTag35yourScored'>
+                            <p className='Tag35'>35/40</p>
+                            <p className='yourScor'>Your Scored</p>
+                        </div>
+                        <div className='elementBox'>
                             <div className=''>
                                 <p><img src='exclamation.png' /></p>
-                                <p>Unattempted: 3</p>
+                                <p className='elementpTag'>Unattempted: 3</p>
                             </div>
+                            <img className='verticalLine' src='VertivalLine.svg' />
                             <div className=''>
                                 <p><img src='Check.png' /></p>
-                                <p>Correct: 35</p>
+                                <p className='elementpTag'>Correct: 35</p>
                             </div>
+                            <img className='verticalLine' src='VertivalLine.svg' />
                             <div className=''>
                                 <p><img src='wrong.png' /></p>
-                                <p>Wrong: 2</p>
+                                <p className='elementpTag'>Wrong: 2</p>
                             </div>
                         </div>
-                    {/* </div> */}
+                    </div>
                     <div className='checkYourAns'>CHECK YOUR ANSWERS</div>
                     <div className='lineScore'></div>
                     <div className='scoreCurrectMaoinDiv'>
